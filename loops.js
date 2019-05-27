60946a259ea9dbbb92d1932abf5b688096d2dae5
thomas lombardo <tomlom1984@hotmail.co.uk>
2019-05-27 15:41:24 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -20,3 +20,13 @@ return "done";
 function doWhileLoop(num) {
   
 }
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 10);
